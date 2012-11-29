2d2b24ed3f82aa1d28f91eb34015411354f63630
Mark Henwood <mark@mcbh.co.uk>
2012-11-29 23:23:31 +0000

    Test runners

---
+++ run_customer_tests.py
@@ -0,0 +1,40 @@
+import os
+from os.path import abspath, join, split
+import subprocess
+import sys
+import time
+
+from behave.__main__ import main as behave_main
+
+PROJECT = 'project'
+
+
+def run(project):
+    print('Starting django testserver')
+    django_process = subprocess.Popen(
+        ['python',
+         '{0}/manage.py'.format(project),
+         'testserver',
+         '--noinput'],
+        stdout=open('/dev/null', 'w'),
+        stderr=open('/dev/null', 'w'))
+    time.sleep(2)
+
+    print('Starting Xvfb')
+    xvfb_process = subprocess.Popen(
+        ['Xvfb', ':99', '-ac', '-screen', '0', '1024x768x8'])
+    os.environ["DISPLAY"] = ":99"
+
+    try:
+        behave_main()  # This calls sys.exit() under all circumstances
+    finally:
+        print('Stopping django testserver')
+        django_process.terminate()
+        print('Stopping Xvfb')
+        xvfb_process.terminate()
+
+
+if __name__ == '__main__':
+    project_dir = split(abspath(__file__))[0]
+    sys.path.insert(0, join(project_dir, PROJECT))
+    run(PROJECT)
---
+++ run_developer_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Developer Test Runner
+#
+PROJECT="project"
+
+echo "Cleaning .pyc files"
+find . -type f -name "*.pyc" -print0 | xargs -r -0 rm
+echo "Running PEP8 checks"
+pep8 --exclude=settings.py,manage.py -r ${PROJECT} features/steps
+if [ $? -ne 0 ]
+then
+    echo "PEP8 violations detected. Stopping run."
+    exit 1
+fi
+echo "Running django-nose tests"
+( cd ${PROJECT} ; python -Wall manage.py test --noinput --exe)
+if [ $? -ne 0 ]
+then
+    echo "django-nose tests failed. Stopping run."
+    exit 1
+fi
+echo "django-nose tests passed"
+
+exit 0
